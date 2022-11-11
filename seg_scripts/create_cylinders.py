f6c59a76b5d0deeb368a4f10a8b6a04c09bb6acc
RosieBarrows <rosie.barrows@kcl.ac.uk>
2022-11-11 15:52:52 +0000

    Autogenerates origin_spacing.json

--- seg_scripts/create_cylinders.py
+++ seg_scripts/create_cylinders.py
@@ -19,6 +19,8 @@ args = parser.parse_args()
 path2points = args.path_to_points
 scriptsPath = "/data/Dropbox/scripts_cemrgapp/seg_scripts/"
 
+os.system("python3 txt_2_json.py "+path2points+"/origin_spacing.txt "+path2points+"/origin_spacing_labels.txt "+path2points+"/origin_spacing.json")
+
 os.system("python3 txt_2_json.py "+path2points+"/points.txt "+path2points+"/labels.txt "+path2points+"/points.json")
 
 seg_corrected_nrrd = path2points+'/seg_corrected.nrrd'
