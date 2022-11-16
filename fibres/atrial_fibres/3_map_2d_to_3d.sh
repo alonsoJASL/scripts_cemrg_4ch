a581b22091df0f0e815e1e4fb383b20a44367f76
RosieBarrows <rosie.barrows@kcl.ac.uk>
2022-11-16 16:49:48 +0000

    Comments out the mapping of tags 30-32

--- fibres/atrial_fibres/3_map_2d_to_3d.sh
+++ fibres/atrial_fibres/3_map_2d_to_3d.sh
@@ -40,42 +40,42 @@ eval $CMD
 
 # ---------------------------------------------------------------------------------
 
-CMD="python main_surf_to_volume_tags.py --meshname ${UACFOLDER}/la/la_fibres_l_sheet
-								   --meshname_uac ${UACFOLDER}/LA_endo/Fibre_endo_l
-								   --tagfiles ${UACFOLDER}/LA_endo/MappedScalar_BB_LA.dat
-								   --tag_labels BB
-								   --default_tag_label LA
-								   --endo_epi_laplace ${UACFOLDER}/la/endo_epi/phie.dat
-								   --outmeshname ${UACFOLDER}/la/la_fibres_l_tags"
-eval $CMD
+# CMD="python main_surf_to_volume_tags.py --meshname ${UACFOLDER}/la/la_fibres_l_sheet
+# 								   --meshname_uac ${UACFOLDER}/LA_endo/Fibre_endo_l
+# 								   --tagfiles ${UACFOLDER}/LA_endo/MappedScalar_BB_LA.dat
+# 								   --tag_labels BB
+# 								   --default_tag_label LA
+# 								   --endo_epi_laplace ${UACFOLDER}/la/endo_epi/phie.dat
+# 								   --outmeshname ${UACFOLDER}/la/la_fibres_l_tags"
+# eval $CMD
 
-CMD="python main_surf_to_volume_tags.py --meshname ${UACFOLDER}/ra/ra_fibres_l_sheet
-								   --meshname_uac ${UACFOLDER}/RA_endo/Fibre_endo_l
-								   --tagfiles ${UACFOLDER}/RA_endo/MappedScalar_BB.dat ${UACFOLDER}/RA_endo/MappedScalar_CT.dat ${UACFOLDER}/RA_endo/MappedScalar_PM.dat
-								   --tag_labels BB CT PM
-								   --tag_labels_endo CT PM
-								   --default_tag_label RA
-								   --endo_epi_laplace ${UACFOLDER}/ra/endo_epi/phie.dat
-								   --outmeshname ${UACFOLDER}/ra/ra_fibres_l_tags"
-eval $CMD
+# CMD="python main_surf_to_volume_tags.py --meshname ${UACFOLDER}/ra/ra_fibres_l_sheet
+# 								   --meshname_uac ${UACFOLDER}/RA_endo/Fibre_endo_l
+# 								   --tagfiles ${UACFOLDER}/RA_endo/MappedScalar_BB.dat ${UACFOLDER}/RA_endo/MappedScalar_CT.dat ${UACFOLDER}/RA_endo/MappedScalar_PM.dat
+# 								   --tag_labels BB CT PM
+# 								   --tag_labels_endo CT PM
+# 								   --default_tag_label RA
+# 								   --endo_epi_laplace ${UACFOLDER}/ra/endo_epi/phie.dat
+# 								   --outmeshname ${UACFOLDER}/ra/ra_fibres_l_tags"
+# eval $CMD
 
 # ---------------------------------------------------------------------------------
 
 echo "Mapping to biatrial mesh..."
 
-CMD="cp ${UACFOLDER}/la/la.nod ${UACFOLDER}/la/la_fibres_l_tags.nod;cp ${UACFOLDER}/la/la.eidx ${UACFOLDER}/la/la_fibres_l_tags.eidx"
+CMD="cp ${UACFOLDER}/la/la.nod ${UACFOLDER}/la/la_fibres_l_sheet.nod;cp ${UACFOLDER}/la/la.eidx ${UACFOLDER}/la/la_fibres_l_sheet.eidx"
 eval $CMD
 
-CMD="cp ${UACFOLDER}/ra/ra.nod ${UACFOLDER}/ra/ra_fibres_l_tags.nod;cp ${UACFOLDER}/ra/ra.eidx ${UACFOLDER}/ra/ra_fibres_l_tags.eidx"
+CMD="cp ${UACFOLDER}/ra/ra.nod ${UACFOLDER}/ra/ra_fibres_l_sheet.nod;cp ${UACFOLDER}/ra/ra.eidx ${UACFOLDER}/ra/ra_fibres_l_sheet.eidx"
 eval $CMD
 
-CMD="meshtool insert submesh -submsh=${UACFOLDER}/la/la_fibres_l_tags -msh=${UACFOLDER}/biatrial/biatrial -ofmt=carp_txt -outmsh=${UACFOLDER}/biatrial/biatrial_la_fibres"
+CMD="meshtool insert submesh -submsh=${UACFOLDER}/la/la_fibres_l_sheet -msh=${UACFOLDER}/biatrial/biatrial -ofmt=carp_txt -outmsh=${UACFOLDER}/biatrial/biatrial_la_fibres"
 eval $CMD
 
 CMD="meshtool convert -imsh=${UACFOLDER}/biatrial/biatrial_la_fibres -omsh=${UACFOLDER}/biatrial/biatrial_la_fibres.vtk"
 eval $CMD
 
-CMD="meshtool insert submesh -submsh=${UACFOLDER}/ra/ra_fibres_l_tags -msh=${UACFOLDER}/biatrial/biatrial_la_fibres -ofmt=carp_txt -outmsh=${UACFOLDER}/biatrial/biatrial_fibres_l"
+CMD="meshtool insert submesh -submsh=${UACFOLDER}/ra/ra_fibres_l_sheet -msh=${UACFOLDER}/biatrial/biatrial_la_fibres -ofmt=carp_txt -outmsh=${UACFOLDER}/biatrial/biatrial_fibres_l"
 eval $CMD
 
 CMD="meshtool convert -imsh=${UACFOLDER}/biatrial/biatrial_fibres_l -omsh=${UACFOLDER}/biatrial/biatrial_fibres_l.vtk"
@@ -88,8 +88,8 @@ echo "Mapping to four-chamber mesh..."
 CMD="cp ${UACFOLDER}/biatrial/biatrial.nod ${UACFOLDER}/biatrial/biatrial_fibres_l.nod;cp ${UACFOLDER}/biatrial/biatrial.eidx ${UACFOLDER}/biatrial/biatrial_fibres_l.eidx"
 eval $CMD
 
-CMD="meshtool insert submesh -submsh=${UACFOLDER}/biatrial/biatrial_fibres_l -msh=${FCH} -ofmt=carp_txt -outmsh=${FCH}_fibres_l"
+CMD="meshtool insert submesh -submsh=${UACFOLDER}/biatrial/biatrial_fibres_l -msh=${FCH} -ofmt=carp_txt -outmsh=${heart_folder}/atrial_fibres/myocardium_fibres_l"
 eval $CMD
 
-CMD="meshtool convert -imsh=${FCH}_fibres_l -omsh=${FCH}_fibres_l.vtk"
+CMD="meshtool convert -imsh=${heart_folder}/atrial_fibres/myocardium_fibres_l -omsh=${heart_folder}/atrial_fibres/myocardium_fibres_l.vtk"
 eval $CMD
