3acd65dd1d9c082724dc157c098822be24132bb7
RosieBarrows <rosie.barrows@kcl.ac.uk>
2022-11-16 16:49:05 +0000

    Changes the tags used to find the base to also remove the LV from the LA

--- UVCs/surfaces_extraction_fibres_la.sh
+++ UVCs/surfaces_extraction_fibres_la.sh
@@ -10,7 +10,7 @@ mkdir $surf_folder
 ################################################################################
 # base
 ################################################################################
-meshtool extract surface -msh=$mesh -surf=${surf_folder}/la.base -ofmt=vtk -op=3:7
+meshtool extract surface -msh=$mesh -surf=${surf_folder}/la.base -ofmt=vtk -op=3:7,1
 
 ################################################################################
 # epicardium, LV endo and RV endo
@@ -19,11 +19,11 @@ meshtool extract surface -msh=$mesh -surf=${surf_folder}/epi_endo -ofmt=vtk -op=
 
 meshtool extract unreachable -msh=${surf_folder}/epi_endo.surfmesh -ifmt=vtk -ofmt=vtk -submsh=${surf_folder}/epi_endo_CC
 
-# python surface_mapping.py ${surf_folder}/epi_endo_CC.part0 ${surf_folder}/epi_endo.surf ${surf_folder}/la.epi ${mesh}
-# python surface_mapping.py ${surf_folder}/epi_endo_CC.part1 ${surf_folder}/epi_endo.surf ${surf_folder}/la.lvendo ${mesh}
+python surface_mapping.py ${surf_folder}/epi_endo_CC.part0 ${surf_folder}/epi_endo.surf ${surf_folder}/la.epi ${mesh}
+python surface_mapping.py ${surf_folder}/epi_endo_CC.part1 ${surf_folder}/epi_endo.surf ${surf_folder}/la.lvendo ${mesh}
 
-python surface_mapping.py ${surf_folder}/epi_endo_CC.part0 ${surf_folder}/epi_endo.surf ${surf_folder}/la.lvendo ${mesh}
-python surface_mapping.py ${surf_folder}/epi_endo_CC.part1 ${surf_folder}/epi_endo.surf ${surf_folder}/la.epi ${mesh}
+# python surface_mapping.py ${surf_folder}/epi_endo_CC.part0 ${surf_folder}/epi_endo.surf ${surf_folder}/la.lvendo ${mesh}
+# python surface_mapping.py ${surf_folder}/epi_endo_CC.part1 ${surf_folder}/epi_endo.surf ${surf_folder}/la.epi ${mesh}
 
 cmd="cp ${heart_folder}/meshing/myocardium_OUT/myocardium.* ${heart_folder}/surfaces_uvc"
 eval $cmd
