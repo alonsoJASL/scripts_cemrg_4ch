d0d15188d69d4598ca07e04c12323f34064af468
Rosie Barrows <rkb23@bioeng433-pc>
2023-07-10 15:10:31 +0100

    Prep for Cristobal

--- seg_scripts/segSmoothing.sh
+++ seg_scripts/segSmoothing.sh
@@ -12,8 +12,8 @@ IMGFILE="${path2points}/seg_final.nrrd"
 OUTFILE="${path2points}/seg_final_smooth"
 VTK_SURF="${path2points}/seg_final_smooth_with_BPs"
 
-cmd="/home/rb21/Software/segtools/bin/segsmooth 
-               -l 1 2 3 4 5 6 7 103 104 105 106 107 201 202 203 204 205 206 207 208 209 210 211 221 222 223 224 225 226 227
+cmd="/home/rb21/Software/segtools/build-b/scripts-2.7/segsmooth 
+                -l 1 2 3 4 5 6 7 103 104 105 106 107 201 202 203 204 205 206 207 208 209 210 211 221 222 223 224 225 226 227
                --fuzz-factor 3.0
                -r $RESOLUTION $RESOLUTION $RESOLUTION
                --padding 0 0 0 
@@ -26,6 +26,6 @@ echo $cmd
 eval $cmd
 
 
-cmd="/home/rb21/Software/segtools/bin/segconvert ${OUTFILE}.nrrd  ${OUTFILE}.inr"
+cmd="/home/rb21/Software/segtools/build-b/scripts-2.7/segconvert ${OUTFILE}.nrrd  ${OUTFILE}.inr"
 echo $cmd
 eval $cmd
