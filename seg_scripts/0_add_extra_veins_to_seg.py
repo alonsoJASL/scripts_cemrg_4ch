e0cf977561111366a1869a4b04648a778e94442a
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2023-08-01 15:03:55 +0100

    Added lines to convert txt to json file

--- seg_scripts/0_add_extra_veins_to_seg.py
+++ seg_scripts/0_add_extra_veins_to_seg.py
@@ -8,16 +8,12 @@ import json
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='To run: python3 create_svc_ivc.py [path_to_points]')
+parser = argparse.ArgumentParser(description='To run: python3 add_extra_veins_to_seg.py [path_to_points]')
 parser.add_argument("path_to_points")
 args = parser.parse_args()
 path2points = args.path_to_points
 
-os.system("python3 txt_2_json.py "+path2points+"/points.txt "+path2points+"/labels.txt "+path2points+"/points.json")
-os.system("python3 txt_2_json.py "+path2points+"/origin_spacing.txt "+path2points+"/origin_spacing_labels.txt "+path2points+"/origin_spacing.json")
-
-
-points_file = open(path2points+'/points.json')
+points_file = open(path2points+'/vein_points.json')
 points_data = json.load(points_file)
 
 # ----------------------------------------------------------------------------------------------
@@ -51,42 +47,48 @@ IVC_label = 14
 # ----------------------------------------------------------------------------------------------
 seg_corrected_nrrd = path2points+'/seg_corrected.nrrd'
 
+# seg_array, header = nrrd.read(seg_corrected_nrrd)
+
+# We read the image with Simple ITK so we don't lose the header information.
+seg_array = sitk.ReadImage(seg_corrected_nrrd)
+
 # ----------------------------------------------------------------------------------------------
 # Give the paths to the SVC/IVC cylinders and the aorta/pulmonary artery slicers
 # Give the associated labels
 # ----------------------------------------------------------------------------------------------
-svc_nrrd = path2points+'/SVC.nrrd'
-ivc_nrrd = path2points+'/IVC.nrrd'
-
-aorta_slicer_nrrd = path2points+'/aorta_slicer.nrrd'
-aorta_slicer_label = 0
-
-PArt_slicer_nrrd = path2points+'/PArt_slicer.nrrd'
-PArt_slicer_label = 0
+LSPV_nrrd = path2points+'/LSPV.nrrd'
+LIPV_nrrd = path2points+'/LIPV.nrrd'
+RSPV_nrrd = path2points+'/RSPV.nrrd'
+RIPV_nrrd = path2points+'/RIPV.nrrd'
+LAA_nrrd = path2points+'/LAA.nrrd'
 
 # ----------------------------------------------------------------------------------------------
 # Convert all of the segmentations to arrays
 # ----------------------------------------------------------------------------------------------
-seg_array = sitk.ReadImage(seg_corrected_nrrd)
+seg_corrected_array, header1 = nrrd.read(seg_corrected_nrrd)
+LSPV_array, header2 = nrrd.read(LSPV_nrrd)
+LIPV_array, header3 = nrrd.read(LIPV_nrrd)
+RSPV_array, header4 = nrrd.read(RSPV_nrrd)
+RIPV_array, header5 = nrrd.read(RIPV_nrrd)
+LAA_array, header6 = nrrd.read(LAA_nrrd)
 
-seg_s2_array, header1 = nrrd.read(seg_corrected_nrrd)
-svc_array, header2 = nrrd.read(svc_nrrd)
-ivc_array, header3 = nrrd.read(ivc_nrrd)
-aorta_slicer_array, header4 = nrrd.read(aorta_slicer_nrrd)
-PArt_slicer_array, header5 = nrrd.read(PArt_slicer_nrrd)
 
 # ----------------------------------------------------------------------------------------------
 # Add the SVC and IVC 
 # ----------------------------------------------------------------------------------------------
-print('\n ## Adding the SVC, IVC and slicers ## \n')
-seg_s2a_array = add_masks_replace_only(seg_s2_array, svc_array, SVC_label,RPV1_label)
-seg_s2a_array = add_masks(seg_s2a_array, ivc_array, IVC_label)
+print('\n ## Adding the extra veins ## \n')
+# seg_corrected_array = add_masks(seg_corrected_array, LSPV_array, LPV1_label)
+seg_corrected_array = add_masks(seg_corrected_array, LIPV_array, LPV2_label)
+# seg_corrected_array = add_masks(seg_corrected_array, RSPV_array, RPV1_label)
+# seg_corrected_array = add_masks(seg_corrected_array, RIPV_array, RPV2_label)
+# seg_corrected_array = add_masks(seg_corrected_array, LAA_array, LAA_label)
+
 
 # ----------------------------------------------------------------------------------------------
 # Format and save the segmentation
 # ----------------------------------------------------------------------------------------------
 print(' ## Formatting and saving the segmentation ##')
-seg_s2a_array = np.swapaxes(seg_s2a_array,0,2)
-# save_itk(seg_s2a_array, origin, spacings, path2points+'/seg_s2a.nrrd')
-save_itk_keeping_header(new_image=seg_s2a_array, original_image=seg_array, filename=path2points+'/seg_s2a.nrrd')
-print(" ## Saved segmentation with SVC/IVC added ##")
+seg_corrected_array = np.swapaxes(seg_corrected_array,0,2)
+# save_itk(seg_corrected_array, origin, spacings, path2points+'/seg_corrected.nrrd')
+save_itk_keeping_header(new_image = seg_corrected_array, original_image=seg_array, filename=path2points+'/seg_corrected.nrrd')
+print(" ## Saved segmentation with extra veins added ##")
