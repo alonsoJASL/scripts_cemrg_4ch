759ec7dae6aa748165a160377b81a108910b4dbc
Jose Alonso Solis Lemus <jsolisle@ic.ac.uk>
2023-08-11 17:00:07 +0100

    refactor_slicers

--- seg_scripts/create_slicers.py
+++ seg_scripts/create_slicers.py
@@ -12,105 +12,27 @@ import multiprocessing as mp
 import json
 import argparse
 
-def cylinder(seg_nrrd,points,plane_name,slicer_radius, slicer_height,origin,spacing):
-	
-	seg_array, header = nrrd.read(seg_nrrd)
-
-	# print(seg_array[:,:,0].shape)
-
-	seg_array_cylinder = np.zeros(seg_array.shape,np.uint8)
-
-	points_coords = copy.deepcopy(points)
-	for i,pts in enumerate(points):
-		points_coords[i,:] = origin+spacing*points[i,:]
-
-	cog = np.mean(points_coords,axis=0)
-
-	v1 = points_coords[1,:]-points_coords[0,:]
-	v2 = points_coords[2,:]-points_coords[0,:]
-	v1 = v1/np.linalg.norm(v1)
-	v2 = v2/np.linalg.norm(v2)
-	n = np.cross(v1,v2)
-	n = n/np.linalg.norm(n)
-
-	p1 = cog - n*slicer_height/2.	
-	p2 = cog + n*slicer_height/2.
-	n = p2-p1
-
-	n_z = seg_array.shape[2]
-	n_x = seg_array.shape[0]
-	n_y = seg_array.shape[1]
-
-	if slicer_height>slicer_radius:
-		cube_size = max(slicer_height,slicer_radius)+10
-	else:
-		cube_size = max(slicer_height,slicer_radius)+30
-
-	print("==========================================================")
-	print("     Constraining the search to a small cube...")
-	print("==========================================================")
-
-	z_cube_coord = []
-	count = 0
-	for i in range(n_z):
-		z = origin[2]+spacing[2]*i
-			
-		distance = np.abs(cog[2]-z)
-		if distance<=cube_size/2.:
-			z_cube_coord.append(i)
-
-	y_cube_coord = []	
-	for i in range(n_y):
-		y = origin[1]+spacing[1]*i
-			
-		distance = np.abs(cog[1]-y)
-		if distance<=cube_size/2.:
-			y_cube_coord.append(i)
-
-	x_cube_coord = []	
-	for i in range(n_x):
-		x = origin[0]+spacing[0]*i
-			
-		distance = np.abs(cog[0]-x)
-		if distance<=cube_size/2.:
-			x_cube_coord.append(i)
-
-	print("============================================================================")
-	print("Generating cylinder of height "+str(slicer_height)+" and radius "+str(slicer_radius)+"...")
-	print("============================================================================")
-
-	for i in x_cube_coord:
-		for j in y_cube_coord:
-			for k in z_cube_coord:
-
-				test_pts = origin+spacing*np.array([i,j,k])
-
-				v1 = test_pts-p1
-				v2 = test_pts-p2
-				if np.dot(v1,n)>=0 and np.dot(v2,n)<=0:
-					test_radius = np.linalg.norm(np.cross(test_pts-p1,n/np.linalg.norm(n)))
-					if test_radius<=slicer_radius:
-						seg_array_cylinder[i,j,k] = seg_array_cylinder[i,j,k]+1
-
-	seg_array_cylinder = np.swapaxes(seg_array_cylinder,0,2)
-
-	print("Saving...")
-	img.save_itk(seg_array_cylinder, origin, spacing, plane_name)
+from txt_2_json import txt2json
+from create_cylinders import cylinder
 
 def main(args) : 
 	path2points = args.path_to_points
-	os.system("python3 txt_2_json.py "+path2points+"/points.txt "+path2points+"/labels.txt "+path2points+"/points.json")
-	os.system("python3 txt_2_json.py "+path2points+"/origin_spacing.txt "+path2points+"/origin_spacing_labels.txt "+path2points+"/origin_spacing.json")
+	points_json = f'{os.path.join(path2points,"points.json")}'
+	origin_spacing_json = f'{os.path.join(path2points,"origin_spacing.json")}'
 
-	file = open(path2points+'/points.json')
-	points_data = json.load(file)
+	txt2json(os.path.join(path2points,"points.txt"), os.path.join(path2points,"labels.txt"), points_json)
+	txt2json(os.path.join(path2points,"origin_spacing.txt"), os.path.join(path2points,"origin_spacing_labels.txt"), origin_spacing_json)
 
-	file = open(path2points+'/origin_spacing.json')
-	origin_data = json.load(file)
+	with open(points_json) as f :
+		points_data = json.load(f)
+
+	with open(origin_spacing_json) as f :
+		origin_data = json.load(f)
+
 	origin = origin_data["origin"]
 	spacing = origin_data["spacing"]
 
-	seg_name = path2points+"/seg_s2a.nrrd"
+	seg_name = os.path.join(path2points, "seg_s2a.nrrd")
 
 	# SVC slicer
 	pts1 = points_data['SVC_slicer_1']
--- seg_scripts/crop_svc_ivc.py
+++ seg_scripts/crop_svc_ivc.py
@@ -1,4 +1,4 @@
-from img import *
+import img
 import SimpleITK as sitk
 
 import numpy as np
@@ -7,39 +7,10 @@ import nrrd
 import json
 import os
 import argparse
-# ----------------------------------------------------------------------------------------------
-# Remake and load points.json
-# ----------------------------------------------------------------------------------------------
-parser = argparse.ArgumentParser(description='To run: python3 crop_svc_ivc.py [path_to_points]')
-parser.add_argument("path_to_points")
-args = parser.parse_args()
-path2points = args.path_to_points
-scriptsPath = "/data/Dropbox/scripts_cemrgapp/seg_scripts/"
 
-os.system("python3 txt_2_json.py "+path2points+"/points.txt "+path2points+"/labels.txt "+path2points+"/points.json")
-points_file = open(path2points+'/points.json')
-points_data = json.load(points_file)
+from txt_2_json import txt2json
 
-# ----------------------------------------------------------------------------------------------
-# Find the origin and spacing
-# ----------------------------------------------------------------------------------------------
-# NOTE - We save the origin and spacings because the "save_itk" function used in
-# the next step makes the format of the header difficult to work with.
-# ----------------------------------------------------------------------------------------------
-origin_spacing_file = open(path2points+'/origin_spacing.json')
-origin_spacing_data = json.load(origin_spacing_file)
-origin = origin_spacing_data['origin']
-spacings = origin_spacing_data['spacing']
-
-# ----------------------------------------------------------------------------------------------
-# Prepare info from json files
-# ----------------------------------------------------------------------------------------------
-
-# SVC
-SVC_seed = points_data['SVC_tip']
-# IVC
-IVC_seed = points_data['IVC_tip']
-
+# constants
 # Ideally this step will be done by reading a json file but 
 # currently can't make sitk.ConnectedThreshold work - 
 # ERROR = argument of type 'uint8_t
@@ -58,107 +29,155 @@ LAA_label = 12
 SVC_label = 13
 IVC_label = 14
 
-# ----------------------------------------------------------------------------------------------
-# Make a folder to hold temporary images
-# ----------------------------------------------------------------------------------------------
-os.system("mkdir "+path2points+"/tmp")
+def main(args):
+    print(" ## Running crop_svc_ivc.py ## ") 
+    path2points = args.path_to_points
+    scriptsPath = "/data/Dropbox/scripts_cemrgapp/seg_scripts/"
+    points_json = os.path.join(path2points, "points.json")
+    origin_spacing_json = os.path.join(path2points, "origin_spacing.json")
 
-# ----------------------------------------------------------------------------------------------
-# Find the origin and spacing
-# ----------------------------------------------------------------------------------------------
-# NOTE - We save the origin and spacings because the "save_itk" function used in
-# the next step makes the format of the header difficult to work with.
-# ----------------------------------------------------------------------------------------------
-origin_spacing_file = open(path2points+'/origin_spacing.json')
-origin_spacing_data = json.load(origin_spacing_file)
-origin = origin_spacing_data['origin']
-spacings = origin_spacing_data['spacing']
+    # ----------------------------------------------------------------------------------------------
+    # Remake and load points.json
+    # ----------------------------------------------------------------------------------------------
+    
+    txt2json(os.path.join(path2points, "points.txt"), os.path.join(path2points, "labels.txt"), points_json)
+    points_file = open(path2points+'/points.json')
+    points_data = json.load(points_file)
 
-# # ----------------------------------------------------------------------------------------------
-# # Slice svc and ivc
-# # ---------------------------------------------------------------------------------------------
-# SVC_slicer_1 = points_data['SVC_slicer_1']
-# SVC_slicer_2 = points_data['SVC_slicer_2']
-# SVC_slicer_3 = points_data['SVC_slicer_3']
-# slicer_points = [SVC_slicer_1[0],SVC_slicer_1[1],SVC_slicer_1[2],SVC_slicer_2[0],SVC_slicer_2[1],SVC_slicer_2[2],SVC_slicer_3[0],SVC_slicer_3[1],SVC_slicer_3[2]]
-# slicer_radius = 30
-# slicer_height = 1
-# mask_plane_creator_alternative(path2points+'/seg_s2a.nrrd',origin,spacings,slicer_points,'SVC_slicer',slicer_radius=slicer_radius,slicer_height=slicer_height,segPath=path2points,scriptsPath=scriptsPath)
+    # ----------------------------------------------------------------------------------------------
+    # Find the origin and spacing
+    # ----------------------------------------------------------------------------------------------
+    # NOTE - We save the origin and spacings because the "save_itk" function used in
+    # the next step makes the format of the header difficult to work with.
+    # ----------------------------------------------------------------------------------------------
+    
+    with open(origin_spacing_json, "r") as f:
+        origin_spacing_data = json.load(f)
 
-# IVC_slicer_1 = points_data['IVC_slicer_1']
-# IVC_slicer_2 = points_data['IVC_slicer_2']
-# IVC_slicer_3 = points_data['IVC_slicer_3']
-# slicer_points = [IVC_slicer_1[0],IVC_slicer_1[1],IVC_slicer_1[2],IVC_slicer_2[0],IVC_slicer_2[1],IVC_slicer_2[2],IVC_slicer_3[0],IVC_slicer_3[1],IVC_slicer_3[2]]
-# slicer_radius = 30
-# slicer_height = 1
-# mask_plane_creator_alternative(path2points+'/seg_s2a.nrrd',origin,spacings,slicer_points,'IVC_slicer',slicer_radius=slicer_radius,slicer_height=slicer_height,segPath=path2points,scriptsPath=scriptsPath)
+    origin = origin_spacing_data['origin']
+    spacings = origin_spacing_data['spacing']
 
-# ----------------------------------------------------------------------------------------------
-# Give the paths to the SVC/IVC cylinders and the aorta/pulmonary artery slicers
-# Give the associated labels
-# ----------------------------------------------------------------------------------------------
-aorta_slicer_nrrd = path2points+'/aorta_slicer.nrrd'
-aorta_slicer_label = 0
+    # ----------------------------------------------------------------------------------------------
+    # Prepare info from json files
+    # ----------------------------------------------------------------------------------------------
 
-PArt_slicer_nrrd = path2points+'/PArt_slicer.nrrd'
-PArt_slicer_label = 0
+    # SVC
+    SVC_seed = points_data['SVC_tip']
+    # IVC
+    IVC_seed = points_data['IVC_tip']
 
-SVC_slicer_nrrd = path2points+'/SVC_slicer.nrrd'
-IVC_slicer_nrrd = path2points+'/IVC_slicer.nrrd'
 
-# ----------------------------------------------------------------------------------------------
-# Convert all of the segmentations to arrays
-# ----------------------------------------------------------------------------------------------
-aorta_slicer_array, header = nrrd.read(aorta_slicer_nrrd)
-PArt_slicer_array, header = nrrd.read(PArt_slicer_nrrd)
-SVC_slicer_array, header = nrrd.read(SVC_slicer_nrrd)
-IVC_slicer_array, header = nrrd.read(IVC_slicer_nrrd)
+    # ----------------------------------------------------------------------------------------------
+    # Make a folder to hold temporary images
+    # ----------------------------------------------------------------------------------------------
+    os.system("mkdir "+path2points+"/tmp")
 
-# ----------------------------------------------------------------------------------------------
-# Remove protruding SVC/IVC and add the slicers
-# ----------------------------------------------------------------------------------------------
-print(' ## Removing any protruding SVC/IVC ## \n')
-seg_s2b_array = connected_component_keep(path2points+'/seg_s2a.nrrd', SVC_seed, SVC_label,path2points)
-seg_s2b_array = np.swapaxes(seg_s2b_array,0,2)
-save_itk(seg_s2b_array, origin, spacings, path2points+'/seg_s2b.nrrd')
-seg_s2c_array = connected_component_keep(path2points+'/seg_s2b.nrrd', IVC_seed, IVC_label,path2points)
-seg_s2c_array = np.swapaxes(seg_s2c_array,0,2)
-save_itk(seg_s2c_array, origin, spacings, path2points+'/seg_s2c.nrrd')
+    # ----------------------------------------------------------------------------------------------
+    # Find the origin and spacing
+    # ----------------------------------------------------------------------------------------------
+    # NOTE - We save the origin and spacings because the "save_itk" function used in
+    # the next step makes the format of the header difficult to work with.
+    # ----------------------------------------------------------------------------------------------
 
-seg_s2c_array, header1 = nrrd.read(path2points+'/seg_s2c.nrrd')
-seg_s2d_array = add_masks_replace_only(seg_s2c_array, aorta_slicer_array, aorta_slicer_label, Ao_BP_label)
-seg_s2d_array = add_masks_replace_only(seg_s2d_array, PArt_slicer_array, PArt_slicer_label, PArt_BP_label)
+    # repetition
+    # origin_spacing_file = open(path2points+'/origin_spacing.json')
+    # origin_spacing_data = json.load(origin_spacing_file)
+    # origin = origin_spacing_data['origin']
+    # spacings = origin_spacing_data['spacing']
 
-new_RA_array = and_filter(seg_s2d_array,SVC_slicer_array,SVC_label,RA_BP_label)
-seg_s2d_array = add_masks_replace_only(seg_s2d_array, new_RA_array, RA_BP_label, SVC_label)
+    # # ----------------------------------------------------------------------------------------------
+    # # Slice svc and ivc
+    # # ---------------------------------------------------------------------------------------------
+    # SVC_slicer_1 = points_data['SVC_slicer_1']
+    # SVC_slicer_2 = points_data['SVC_slicer_2']
+    # SVC_slicer_3 = points_data['SVC_slicer_3']
+    # slicer_points = [SVC_slicer_1[0],SVC_slicer_1[1],SVC_slicer_1[2],SVC_slicer_2[0],SVC_slicer_2[1],SVC_slicer_2[2],SVC_slicer_3[0],SVC_slicer_3[1],SVC_slicer_3[2]]
+    # slicer_radius = 30
+    # slicer_height = 1
+    # mask_plane_creator_alternative(path2points+'/seg_s2a.nrrd',origin,spacings,slicer_points,'SVC_slicer',slicer_radius=slicer_radius,slicer_height=slicer_height,segPath=path2points,scriptsPath=scriptsPath)
 
-new_RA_array = and_filter(seg_s2d_array,IVC_slicer_array,IVC_label,RA_BP_label)
-seg_s2d_array = add_masks_replace_only(seg_s2d_array, new_RA_array, RA_BP_label, IVC_label)
+    # IVC_slicer_1 = points_data['IVC_slicer_1']
+    # IVC_slicer_2 = points_data['IVC_slicer_2']
+    # IVC_slicer_3 = points_data['IVC_slicer_3']
+    # slicer_points = [IVC_slicer_1[0],IVC_slicer_1[1],IVC_slicer_1[2],IVC_slicer_2[0],IVC_slicer_2[1],IVC_slicer_2[2],IVC_slicer_3[0],IVC_slicer_3[1],IVC_slicer_3[2]]
+    # slicer_radius = 30
+    # slicer_height = 1
+    # mask_plane_creator_alternative(path2points+'/seg_s2a.nrrd',origin,spacings,slicer_points,'IVC_slicer',slicer_radius=slicer_radius,slicer_height=slicer_height,segPath=path2points,scriptsPath=scriptsPath)
 
-# ----------------------------------------------------------------------------------------------
-# Format and save the segmentation
-# ----------------------------------------------------------------------------------------------
-print(' ## Formatting and saving the segmentation ##')
-seg_s2d_array = np.swapaxes(seg_s2d_array,0,2)
-save_itk(seg_s2d_array, origin, spacings, path2points+'/seg_s2d.nrrd')
-print(" ## Saved segmentation with SVC/IVC added and aorta/pulmonary artery cropped ##")
+    # ----------------------------------------------------------------------------------------------
+    # Give the paths to the SVC/IVC cylinders and the aorta/pulmonary artery slicers
+    # Give the associated labels
+    # ----------------------------------------------------------------------------------------------
+    aorta_slicer_nrrd = os.path.join(path2points, 'aorta_slicer.nrrd')
+    aorta_slicer_label = 0
 
-# ----------------------------------------------------------------------------------------------
-# Flattening base of SVC/IVC
-# ----------------------------------------------------------------------------------------------
-print(' ## Flattening base of SVC/IVC ## \n')
-seg_s2e_array = connected_component(path2points+'/seg_s2d.nrrd', SVC_seed, SVC_label,path2points)
-seg_s2e_array = add_masks_replace_only(seg_s2e_array,seg_s2e_array,RA_BP_label,SVC_label)
-CC_array, header = nrrd.read(path2points+'/tmp/CC.nrrd')
-seg_s2e_array = add_masks_replace(seg_s2e_array, CC_array, SVC_label)
+    PArt_slicer_nrrd = os.path.join(path2points, 'PArt_slicer.nrrd')
+    PArt_slicer_label = 0
 
-seg_s2e_array = np.swapaxes(seg_s2e_array,0,2)
-save_itk(seg_s2e_array, origin, spacings, path2points+'/seg_s2e.nrrd')
+    SVC_slicer_nrrd = os.path.join(path2points, 'SVC_slicer.nrrd')
+    IVC_slicer_nrrd = os.path.join(path2points, 'IVC_slicer.nrrd')
 
-seg_s2f_array = connected_component(path2points+'/seg_s2e.nrrd', IVC_seed, IVC_label,path2points)
-seg_s2f_array = add_masks_replace_only(seg_s2f_array,seg_s2f_array,RA_BP_label,IVC_label)
-CC_array, header = nrrd.read(path2points+'/tmp/CC.nrrd')
-seg_s2f_array = add_masks_replace(seg_s2f_array, CC_array, IVC_label)
+    # ----------------------------------------------------------------------------------------------
+    # Convert all of the segmentations to arrays
+    # ----------------------------------------------------------------------------------------------
+    aorta_slicer_array, header = nrrd.read(aorta_slicer_nrrd)
+    PArt_slicer_array, header = nrrd.read(PArt_slicer_nrrd)
+    SVC_slicer_array, header = nrrd.read(SVC_slicer_nrrd)
+    IVC_slicer_array, header = nrrd.read(IVC_slicer_nrrd)
 
-seg_s2f_array = np.swapaxes(seg_s2f_array,0,2)
-save_itk(seg_s2f_array, origin, spacings, path2points+'/seg_s2f.nrrd')
+    # ----------------------------------------------------------------------------------------------
+    # Remove protruding SVC/IVC and add the slicers
+    # ----------------------------------------------------------------------------------------------
+    print(' ## Removing any protruding SVC/IVC ## \n')
+    seg_s2b_array = img.connected_component_keep(path2points+'/seg_s2a.nrrd', SVC_seed, SVC_label,path2points)
+    seg_s2b_array = np.swapaxes(seg_s2b_array,0,2)
+    img.save_itk(seg_s2b_array, origin, spacings, path2points+'/seg_s2b.nrrd')
+    
+    seg_s2c_array = img.connected_component_keep(path2points+'/seg_s2b.nrrd', IVC_seed, IVC_label,path2points)
+    seg_s2c_array = np.swapaxes(seg_s2c_array,0,2)
+    img.save_itk(seg_s2c_array, origin, spacings, path2points+'/seg_s2c.nrrd')
+
+    seg_s2c_array, header1 = nrrd.read(path2points+'/seg_s2c.nrrd')
+    seg_s2d_array = img.add_masks_replace_only(seg_s2c_array, aorta_slicer_array, aorta_slicer_label, Ao_BP_label)
+    seg_s2d_array = img.add_masks_replace_only(seg_s2d_array, PArt_slicer_array, PArt_slicer_label, PArt_BP_label)
+
+    new_RA_array = img.and_filter(seg_s2d_array,SVC_slicer_array,SVC_label,RA_BP_label)
+    seg_s2d_array = img.add_masks_replace_only(seg_s2d_array, new_RA_array, RA_BP_label, SVC_label)
+
+    new_RA_array = img.and_filter(seg_s2d_array,IVC_slicer_array,IVC_label,RA_BP_label)
+    seg_s2d_array = img.add_masks_replace_only(seg_s2d_array, new_RA_array, RA_BP_label, IVC_label)
+
+    # ----------------------------------------------------------------------------------------------
+    # Format and save the segmentation
+    # ----------------------------------------------------------------------------------------------
+    print(' ## Formatting and saving the segmentation ##')
+    seg_s2d_array = np.swapaxes(seg_s2d_array,0,2)
+    img.save_itk(seg_s2d_array, origin, spacings, path2points+'/seg_s2d.nrrd')
+    print(" ## Saved segmentation with SVC/IVC added and aorta/pulmonary artery cropped ##")
+
+    # ----------------------------------------------------------------------------------------------
+    # Flattening base of SVC/IVC
+    # ----------------------------------------------------------------------------------------------
+    print(' ## Flattening base of SVC/IVC ## \n')
+    seg_s2e_array = img.connected_component(path2points+'/seg_s2d.nrrd', SVC_seed, SVC_label,path2points)
+    seg_s2e_array = img.add_masks_replace_only(seg_s2e_array,seg_s2e_array,RA_BP_label,SVC_label)
+    CC_array, header = nrrd.read(path2points+'/tmp/CC.nrrd')
+    seg_s2e_array = img.add_masks_replace(seg_s2e_array, CC_array, SVC_label)
+
+    seg_s2e_array = np.swapaxes(seg_s2e_array,0,2)
+    img.save_itk(seg_s2e_array, origin, spacings, path2points+'/seg_s2e.nrrd')
+
+    seg_s2f_array = img.connected_component(path2points+'/seg_s2e.nrrd', IVC_seed, IVC_label,path2points)
+    seg_s2f_array = img.add_masks_replace_only(seg_s2f_array,seg_s2f_array,RA_BP_label,IVC_label)
+    CC_array, header = nrrd.read(path2points+'/tmp/CC.nrrd')
+    seg_s2f_array = img.add_masks_replace(seg_s2f_array, CC_array, IVC_label)
+
+    seg_s2f_array = np.swapaxes(seg_s2f_array,0,2)
+    img.save_itk(seg_s2f_array, origin, spacings, path2points+'/seg_s2f.nrrd')
+
+if __name__ == '__main__': 
+    parser = argparse.ArgumentParser(description='To run: python3 crop_svc_ivc.py [path_to_points]')
+    parser.add_argument("path_to_points")
+    args = parser.parse_args()
+
+    main(args)
