8dece22ed7af476f9e5c364e1992b3947ae5ac5f
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2023-08-14 09:06:50 +0100

    Removed the saving of the surfaces

--- seg_scripts/segSmoothing.sh
+++ seg_scripts/segSmoothing.sh
@@ -10,15 +10,13 @@ path2points=$1
 
 IMGFILE="${path2points}/seg_final.nrrd"
 OUTFILE="${path2points}/seg_final_smooth"
-VTK_SURF="${path2points}/seg_final_smooth_with_BPs"
 
-cmd="/home/rb21/Software/segtools/build-b/scripts-2.7/segsmooth 
+cmd="/home/common/carpusers-segtools/bin/segsmooth
                 -l 1 2 3 4 5 6 7 103 104 105 106 107 201 202 203 204 205 206 207 208 209 210 211 221 222 223 224 225 226 227
                --fuzz-factor 3.0
                -r $RESOLUTION $RESOLUTION $RESOLUTION
                --padding 0 0 0 
                --voxel-units
-	       --save-surfaces $VTK_SURF
            $IMGFILE
                ${OUTFILE}.nrrd
                "
@@ -26,6 +24,6 @@ echo $cmd
 eval $cmd
 
 
-cmd="/home/rb21/Software/segtools/build-b/scripts-2.7/segconvert ${OUTFILE}.nrrd  ${OUTFILE}.inr"
+cmd="/home/common/carpusers-segtools/bin/segconvert ${OUTFILE}.nrrd  ${OUTFILE}.inr"
 echo $cmd
 eval $cmd
