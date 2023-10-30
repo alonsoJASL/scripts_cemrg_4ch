d1cb93f5a76fda00052edbbb9026f19963ecf4cb
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2023-10-30 17:19:49 +0000

    Optimised cylinder function

--- seg_scripts/1_create_cylinders.py
+++ seg_scripts/1_create_cylinders.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 import nrrd
-import copy
 import numpy as np
 import os
 from img import *
-import multiprocessing as mp
 import json
 import argparse
 import tqdm
@@ -78,8 +76,7 @@ def cylinder(seg_nrrd, points, plane_name, slicer_radius, slicer_height, origin,
     seg_array_cylinder = np.swapaxes(seg_array_cylinder, 0, 2)
 
     print("Saving...")
-    save_itk(seg_array_cylinder, origin, spacing, plane_name)
-
+    save_itk_keeping_header(seg_array_cylinder, origin, spacing, plane_name)
 
 
 file = open(path2points+'/points.json')
