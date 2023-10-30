d6bd282a817e315fe5815bb479ec5ebdd7e7ccd0
Cristobal Rodero <cristobal.rodero@imperial.ac.uk>
2023-10-30 16:23:03 +0000

    Optimised cylinder creation

--- seg_scripts/1_create_cylinders.py
+++ seg_scripts/1_create_cylinders.py
@@ -3,14 +3,11 @@ import nrrd
 import copy
 import numpy as np
 import os
-import SimpleITK as sitk
-import string
 from img import *
-import subprocess
-import time
 import multiprocessing as mp
 import json
 import argparse
+import tqdm
 
 parser = argparse.ArgumentParser(description='To run: python3 create_cylinders.py [path_to_points]')
 parser.add_argument("path_to_points")
@@ -22,90 +19,66 @@ os.system("python3 txt_2_json.py "+path2points+"/points.txt "+path2points+"/labe
 os.system("python3 txt_2_json.py "+path2points+"/origin_spacing.txt "+path2points+"/origin_spacing_labels.txt "+path2points+"/origin_spacing.json")
 
 
-def cylinder(seg_nrrd,points,plane_name,slicer_radius, slicer_height,origin,spacing):
-	
-	seg_array, header = nrrd.read(seg_nrrd)
+def cylinder(seg_nrrd, points, plane_name, slicer_radius, slicer_height, origin, spacing):
+    seg_array, header = nrrd.read(seg_nrrd)
 
-	# print(seg_array[:,:,0].shape)
+    # Precompute values
+    points_coords = origin + spacing * points
+    cog = np.mean(points_coords, axis=0)
 
-	seg_array_cylinder = np.zeros(seg_array.shape,np.uint8)
+    v1 = points_coords[1] - points_coords[0]
+    v2 = points_coords[2] - points_coords[0]
+    v1 = v1 / np.linalg.norm(v1)
+    v2 = v2 / np.linalg.norm(v2)
+    n = np.cross(v1, v2)
+    n = n / np.linalg.norm(n)
 
-	points_coords = copy.deepcopy(points)
-	for i,pts in enumerate(points):
-		points_coords[i,:] = origin+spacing*points[i,:]
+    p1 = cog - n * slicer_height / 2.
+    p2 = cog + n * slicer_height / 2.
 
-	cog = np.mean(points_coords,axis=0)
+    n_z, n_x, n_y = seg_array.shape
 
-	v1 = points_coords[1,:]-points_coords[0,:]
-	v2 = points_coords[2,:]-points_coords[0,:]
-	v1 = v1/np.linalg.norm(v1)
-	v2 = v2/np.linalg.norm(v2)
-	n = np.cross(v1,v2)
-	n = n/np.linalg.norm(n)
+    cube_size = max(slicer_height, slicer_radius) + 10 if slicer_height > slicer_radius else max(slicer_height, slicer_radius) + 30
 
-	p1 = cog - n*slicer_height/2.	
-	p2 = cog + n*slicer_height/2.
-	n = p2-p1
+    print("==========================================================")
+    print("     Constraining the search to a small cube...")
+    print("==========================================================")
 
-	n_z = seg_array.shape[2]
-	n_x = seg_array.shape[0]
-	n_y = seg_array.shape[1]
+    z_coords = origin[2] + spacing[2] * np.arange(n_z)
+    z_cube_coord = np.where(np.abs(cog[2] - z_coords) <= cube_size / 2.)[0]
 
-	if slicer_height>slicer_radius:
-		cube_size = max(slicer_height,slicer_radius)+10
-	else:
-		cube_size = max(slicer_height,slicer_radius)+30
+    y_coords = origin[1] + spacing[1] * np.arange(n_y)
+    y_cube_coord = np.where(np.abs(cog[1] - y_coords) <= cube_size / 2.)[0]
 
-	print("==========================================================")
-	print("     Constraining the search to a small cube...")
-	print("==========================================================")
+    x_coords = origin[0] + spacing[0] * np.arange(n_x)
+    x_cube_coord = np.where(np.abs(cog[0] - x_coords) <= cube_size / 2.)[0]
 
-	z_cube_coord = []
-	count = 0
-	for i in range(n_z):
-		z = origin[2]+spacing[2]*i
-			
-		distance = np.abs(cog[2]-z)
-		if distance<=cube_size/2.:
-			z_cube_coord.append(i)
+    print("============================================================================")
+    print("Generating cylinder of height " + str(slicer_height) + " and radius " + str(slicer_radius) + "...")
+    print("============================================================================")
 
-	y_cube_coord = []	
-	for i in range(n_y):
-		y = origin[1]+spacing[1]*i
-			
-		distance = np.abs(cog[1]-y)
-		if distance<=cube_size/2.:
-			y_cube_coord.append(i)
+    seg_array_cylinder = np.zeros(seg_array.shape, dtype=np.uint8)
 
-	x_cube_coord = []	
-	for i in range(n_x):
-		x = origin[0]+spacing[0]*i
-			
-		distance = np.abs(cog[0]-x)
-		if distance<=cube_size/2.:
-			x_cube_coord.append(i)
+    with tqdm.tqdm(total=len(x_cube_coord)) as pbar:
+        for i in x_cube_coord:
+            for j in y_cube_coord:
+                for k in z_cube_coord:
+                    test_pts = origin + spacing * np.array([i, j, k])
+                    v1 = test_pts - p1
+                    v2 = test_pts - p2
+                    dot_v1 = np.dot(v1, n)
+                    dot_v2 = np.dot(v2, n)
+                    cross_norm = np.linalg.norm(np.cross(test_pts - p1, n / np.linalg.norm(n)))
 
-	print("============================================================================")
-	print("Generating cylinder of height "+str(slicer_height)+" and radius "+str(slicer_radius)+"...")
-	print("============================================================================")
+                    if dot_v1 >= 0 and dot_v2 <= 0 and cross_norm <= slicer_radius:
+                        seg_array_cylinder[i, j, k] += 1
+		
+            pbar.update(1)  # Update the progress bar
 
-	for i in x_cube_coord:
-		for j in y_cube_coord:
-			for k in z_cube_coord:
+    seg_array_cylinder = np.swapaxes(seg_array_cylinder, 0, 2)
 
-				test_pts = origin+spacing*np.array([i,j,k])
-
-				v1 = test_pts-p1
-				v2 = test_pts-p2
-				if np.dot(v1,n)>=0 and np.dot(v2,n)<=0:
-					test_radius = np.linalg.norm(np.cross(test_pts-p1,n/np.linalg.norm(n)))
-					if test_radius<=slicer_radius:
-						seg_array_cylinder[i,j,k] = seg_array_cylinder[i,j,k]+1
-
-	seg_array_cylinder = np.swapaxes(seg_array_cylinder,0,2)
-
-	print("Saving...")
-	save_itk(seg_array_cylinder, origin, spacing, plane_name)
+    print("Saving...")
+    save_itk(seg_array_cylinder, origin, spacing, plane_name)
 
 
 
